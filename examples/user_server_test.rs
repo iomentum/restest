637eae8df46a3bb5de16b225960acdeacfcd4803
Sasha Pourcelot <sasha.pourcelot@iomentum.com>
2021-10-11 21:07:06 +0200

    Add documentation for `user_server_test` example

--- examples/user_server_test.rs
+++ examples/user_server_test.rs
@@ -1,22 +1,64 @@
+//! # User server test example
+//!
+//! *To find the quick start commands, scroll at the bottom of the
+//! documentation.*
+//!
+//! ## Description
+//!
+//! We're testing a simple user database REST API using `restest`-provided
+//! macros.
+//!
+//! ## Running
+//!
+//! The server must be started in a terminal, so that the testing code can query
+//! it. The server never ends, so it must be stopped by pressing Ctrl + C.
+//!
+//! ## Commands
+//!
+//! $ cargo run --example user_server
+//! $ cargo test --example user_server_test
+
 #![feature(assert_matches)]
 
 use restest::assert_api;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+/// The data we send to the server when using the `PUT` route.
+///
+/// This does not need to be *exactly* the same as the datatype defined in
+/// `user_server`. The only constraint is that this must `Serialize` to a JSON
+/// body that is accepted by the server.
 #[derive(Serialize)]
 struct UserInput {
     year_of_birth: usize,
 }
 
+/// The data that the server sends us back when we add an user.
+///
+/// Once again, the only constraint is that we must deserialize what the server
+/// responds to us.
 #[derive(Debug, Deserialize)]
 struct User {
     year_of_birth: usize,
     id: Uuid,
 }
 
+/// A simple test for the PUT route.
+///
+/// We send a simple request adding a new user to the database, and tell what
+/// we expect as a response.
 #[tokio::test]
 pub async fn put_simple() {
+    /// The first macro argument is the type of request, followed by the URL.
+    ///
+    /// Next to it are the input and expected output. The input is a plain
+    /// expression that must be `Serialize` and the output is a *pattern* that
+    /// is compared with the response body.
+    ///
+    /// We're using a pattern instead of a regular expression so that some
+    /// fields can be omitted. Similarly, we can include a specific variant
+    /// without caring about the others.
     assert_api! {
         POST "/users",
         UserInput {
