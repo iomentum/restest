6c4512725015ab7db9cf98c0be8b6e7f27807003
Sasha Pourcelot <sasha.pourcelot@iomentum.com>
2021-10-30 23:32:04 +0200

    More doc

--- src/request.rs
+++ src/request.rs
@@ -1,3 +1,5 @@
+//! An HTTP request we're about to run.
+
 use std::collections::HashMap;
 
 use http::status::StatusCode;
@@ -6,6 +8,46 @@ use serde::{de::DeserializeOwned, Serialize};
 
 use crate::url::IntoUrl;
 
+/// An HTTP request we're about to run.
+///
+/// # Creating a request
+///
+/// First a [`RequestBuilder`] must be created. This object will allow to
+/// encode the request information. It can be created with [`Request::get`]
+/// or [`Request::post`], depending on the kind of request needed.
+///
+/// Then, various request metadata can be encoded in the builder once it is
+/// created. For instance, one can use the
+/// [`with_header`](RequestBuilder::with_header) method to specify a header key
+/// to the request.
+///
+/// After every metadata is encoded, the
+/// [`with_body`](RequestBuilder::with_body) method allows to specify a body and
+/// create the final [`Request`] object.
+///
+/// The following code snippet shows all these three steps:
+///
+/// ```rust
+/// use restest::Request;
+///
+/// use serde::Serialize;
+///
+/// let request = Request::get("users")       // Creating the builder...
+///     .with_header("token", "mom-said-yes") // ... Adding metadata to the builder
+///     .with_body(GetUsersFilter::All);      // ... Adding a body, building the final Request.
+///
+/// #[derive(Serialize)]
+/// enum GetUsersFilter {
+///     All,
+///     YoungerThan(u8),
+///     OlderThan(u8),
+/// }
+/// ```
+///
+/// # Running a request
+///
+/// Once the [`Request`] has been successfully created, it can be run by using
+/// the [`Context::run`](crate::Context::run) method.
 pub struct Request<B>
 where
     B: Serialize,
@@ -17,6 +59,24 @@ where
 }
 
 impl Request<()> {
+    /// Creates a GET request builder for a specific URL.
+    ///
+    /// # Specifying an URL
+    ///
+    /// The url argument must be either a string literal or the value produced
+    /// by the [`path`] macro. Only the absolute path to the resource must be
+    /// passed.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use restest::{path, Request};
+    ///
+    /// let request_1 = Request::get("users");
+    ///
+    /// let user_name = "scrabsha";
+    /// let request_2 = Request::get(path!["users", user_name]);
+    /// ```
     pub fn get(url: impl IntoUrl) -> RequestBuilder {
         let url = url.into_url();
         RequestBuilder {
@@ -26,6 +86,16 @@ impl Request<()> {
         }
     }
 
+    /// Creates a POST request builder for a specific URL.
+    ///
+    /// # Specifying an URL
+    ///
+    /// The url argument must be either a string literal or the value produced
+    /// by the [`path`] macro. Only the absolute path to the resource must be
+    /// passed.
+    ///
+    /// Refer to the [`get`][Request::get] method documentation for a
+    /// self-describing example.
     pub fn post(url: impl IntoUrl) -> RequestBuilder {
         let url = url.into_url();
         RequestBuilder {
