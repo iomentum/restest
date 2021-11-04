2416e49d67b707908a057761a5634ce0a49fbcfb
Sasha Pourcelot <sasha.pourcelot@iomentum.com>
2021-11-04 13:59:33 +0100

    Update tests!

--- tests/ok/json_pattern_array.rs
+++ tests/ok/json_pattern_array.rs
@@ -1,16 +1,23 @@
 fn main() {
-    restest::assert_body_matches!([42, 41], [42, 41]);
+    restest::assert_body_matches!(vec![42, 41], [42, 41]);
 
     restest::assert_body_matches! {
-        [42, 101],
+        vec![42, 101],
         a
     };
 
     assert_eq!(a, [42, 101]);
 
     restest::assert_body_matches! {
-        [101, 42],
+        vec![101, 42],
         [a, 42],
+    };
+
+    assert_eq!(a, 101);
+
+    restest::assert_body_matches! {
+        vec![101, 102],
+        [a, _],
     };
 
     assert_eq!(a, 101);
